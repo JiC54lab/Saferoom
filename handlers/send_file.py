78db5a36c8f223989ff72b9a9d8497662d270879
JiC54 <jic54studio@gmail.com>
2022-08-31 11:48:04 +0200

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -34,6 +34,14 @@ async def media_forward(bot: Client, user_id: int, file_id: int):
 
 
 async def send_media_and_reply(bot: Client, user_id: int, file_id: int):
-    sent_message = await media_forward(bot, user_id, file_id)
+    sent_message = await media_forward(bot, user_id, file_id, 
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("🎞MOVIES🎞", url="https://t.me/+K1Z4kS0Cf8c3ZDU0"),
+                        InlineKeyboardButton("📺SERIES📺", url="https://t.me/+DOvLtPVH3wllNDZk")
+                    ]
+                ]
+            ))
     await reply_forward(message=sent_message, file_id=file_id)
     await asyncio.sleep(2)
