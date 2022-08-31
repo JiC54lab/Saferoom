d119cbc628ce95a1f55bbfb614064a4281b1d934
JiC54lab <105932652+JiC54lab@users.noreply.github.com>
2022-08-31 02:17:13 +0200

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -28,23 +28,9 @@ async def reply_forward(message: Message, file_id: int):
 
 
 async def media_forward(bot: Client, user_id: int, file_id: int):
-        if Config.FORWARD_AS_COPY is True:
-            return await bot.send_message('Decrypting file...\n**ETA** ```1m13s```')
- 
-        rju1 = await f"Decrypting file...\n**ETA** ```1m13s```".edit('Decrypting file...\n**ETA** ```41s```')
-
-        rju2 = await rju1.edit('Decrypting file...\n**ETA** ```19s```')
-
-        rju3 = await rju2.edit('Decrypting file...\n**ETA** ```4s```')
-        rju4 = await rju3.edit('Decryption Complete! Uploading starts...⏳')
-        rju5 = await rju4.edit('Decryption Complete! Uploading starts...⌛️')
-        rju6 = await rju5.edit('Uploading\n▣▣▢▢▢▢ 27%')
-        rju7 = await rju6.edit('Uploading\n▣▣▣▢▢▢ 50%')
-        rju8 = await rju7.edit('Uploading\n▣▣▣▣▢▢ 66%')
-        rju9 = await rju8.edit('Uploading\n▣▣▣▣▣▢ 83%')
-        rju10 = await rju9.edit('Uploading\n▣▣▣▣▣▣ 97%')
-        await asyncio.sleep(2)
-        await rju10.delete()
+try:
+    if Config.FORWARD_AS_COPY is True:
+        return
         await bot.copy_message(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
                                           message_id=file_id,
                                           reply_markup=InlineKeyboardMarkup(
@@ -56,7 +42,12 @@ async def media_forward(bot: Client, user_id: int, file_id: int):
             )
                                          )
                                          
-
+elif Config.FORWARD_AS_COPY is False:
+            return await bot.forward_messages(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
+                                              message_ids=file_id)
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+        return media_forward(bot, user_id, file_id)
 
 async def send_media_and_reply(bot: Client, user_id: int, file_id: int):
     sent_message = await media_forward(bot, user_id, file_id)
